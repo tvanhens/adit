f4d12650a435f461effb52f0cdf6cc61d222eb43
Tyler van Hensbergen <tvanhens@gmail.com>
2015-08-08 12:17:31 -0400

    Nrepl server/client test fixture

--- test/adit/core_test.clj
+++ test/adit/core_test.clj
@@ -1,7 +1,28 @@
-(ns adit.core-test
+(ns adit.core-testx
   (:require [clojure.test :refer :all]
-            [adit.core :refer :all]))
+            [adit.core :refer :all]
+            [clojure.tools.nrepl.server :as server]
+            [clojure.tools.nrepl :as repl]
+            [clojure.tools.nrepl.transport :as t]
+            [clojure.tools.nrepl.misc :refer [response-for]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def nrepl-port 9000)
+
+(defn handler [{:keys [op transport] :as msg}]
+  (t/send transport (response-for msg :status :done :value 5)))
+
+(defn nrepl-server [f]
+  (let [s (server/start-server :port nrepl-port :handler handler)]
+    (f)
+    (.close s)))
+
+(use-fixtures :once nrepl-server)
+
+(deftest nrepl-behavior
+  (testing "nrepl should run clojure code remotely"
+    (with-open [conn (repl/connect :port 9000)]
+      (let [client (repl/client conn 1000)]
+        (-> client
+            (repl/message {:op :eval :code "(in-ns 'adit.core-test)"})
+            doall
+            clojure.pprint/pprint)))))
