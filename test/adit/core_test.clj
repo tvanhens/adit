8830cf85e54f588237277793321c606fb569c001
Tyler van Hensbergen <tvanhens@gmail.com>
2015-08-08 17:37:52 -0400

    Disable core test

--- test/adit/core_test.clj
+++ test/adit/core_test.clj
@@ -6,23 +6,24 @@
             [clojure.tools.nrepl.transport :as t]
             [clojure.tools.nrepl.misc :refer [response-for]]))
 
-(def nrepl-port 9000)
+(comment
+  (def nrepl-port 9000)
 
-(defn handler [{:keys [op transport] :as msg}]
-  (t/send transport (response-for msg :status :done :value 5)))
+  (defn handler [{:keys [op transport] :as msg}]
+    (t/send transport (response-for msg :status :done :value 5)))
 
-(defn nrepl-server [f]
-  (let [s (server/start-server :port nrepl-port :handler handler)]
-    (f)
-    (.close s)))
+  (defn nrepl-server [f]
+    (let [s (server/start-server :port nrepl-port :handler handler)]
+      (f)
+      (.close s)))
 
-(use-fixtures :once nrepl-server)
+  (use-fixtures :once nrepl-server)
 
-(deftest nrepl-behavior
-  (testing "nrepl should run clojure code remotely"
-    (with-open [conn (repl/connect :port 9000)]
-      (let [client (repl/client conn 1000)]
-        (-> client
-            (repl/message {:op :eval :code "(in-ns 'adit.core-test)"})
-            doall
-            clojure.pprint/pprint)))))
+  (deftest nrepl-behavior
+    (testing "nrepl should run clojure code remotely"
+      (with-open [conn (repl/connect :port 9000)]
+        (let [client (repl/client conn 1000)]
+          (-> client
+              (repl/message {:op :eval :code "(in-ns 'adit.core-test)"})
+              doall
+              clojure.pprint/pprint))))))
